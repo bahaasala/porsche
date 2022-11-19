800c0d32743e86c1cd17c8d9cb5330a97435b121
bahaasala <salaymeh1999@live.com>
2022-11-19 13:52:00 +0100

    navigation bar animation added

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,11 @@
-// JavaScript Document
-console.log("hi");
+// navigation menu animation
+var car = document.querySelectorAll(".car");
 
-
-// var _nav = $(".menu-wrapper");
-// var _nav_offset = _nav.offset();
-// var _nav_height = _nav.outerHeight();
-// var wST = $(window).scrollTop();
-
-// $('#menu-dropdown').css({
-//     'top': _nav_offset.top - wST + _nav_height
-// })
-
-var $ = jQuery;
-
-var menuitem = document.querySelector(".car");
-// menuitem.addEventListener('mouseover', showmenu)
-console.log(menuitem);
-
-
-// menuitem.addEventListener('mouseleave', hidemenu)
-
-
-// for (let i = 0; i < 5; i++) {
-//     console.log('test');
-// }
-var cars = document.querySelectorAll(".car");
-
-for (var i = 0; i < cars.length; i++) {
-    cars[i].addEventListener("mouseover", function () {
-        $('#menu-dropdown').stop().slideDown();
-    });
-    cars[i].addEventListener("mouseleave", function () {
-        $('#menu-dropdown').slideUp();
-    });
-}
-
-
-// function showmenu() {
-//     // console.log("over");
-//     $('#menu-dropdown').stop().slideDown();
-// }
-
-// function hidemenu() {
-//     $('#menu-dropdown').slideUp();
-// }
-
-// jQuery(document).ready(function () {
-//     // if(){
-//     //     console.log(test);
-//     // };
-
-
-//     $(".menu-wrapper li.test").mouseenter(function () {
-//         $('#menu-dropdown').stop().slideDown();
-//     });
-
-//     $(".menu-wrapper li.test, #menu-dropdown").mouseleave(function () {
-//         if (!$('#menu-dropdown').is(':hover')) {
-//             $('#menu-dropdown').slideUp();
-//         };
-//     });
-
-// });
+car.forEach(item =>
+    item.addEventListener('mouseover', event =>
+        item.classList.add('dropdown-active'))
+);
+car.forEach(item =>
+    item.addEventListener('mouseleave', event =>
+        item.classList.remove('dropdown-active'))
+);
